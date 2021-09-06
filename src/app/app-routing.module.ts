6e8f3cf395536b118e47a36d2ae0bd901f56faa1
Juanan <jantonio.perez@estudioclandestino.es>
2021-09-06 15:45:21 +0200

    Primera tarea finalizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailProductComponent } from './components/detail-product/detail-product.component';
+import { Error404Component } from './components/error404/error404.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: 'full', redirectTo: "/home" },
+  { path: "home", component: HomeComponent },
+  { path: "products-list", component: ProductsListComponent },
+  { path: "product/:idproduct", component: DetailProductComponent },
+  { path: "**", component: Error404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
